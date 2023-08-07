7a9109534b3c77310ce7c6cafaab1a6ec4a25e86
jose90qi3io <81458974+jose90qi3io@users.noreply.github.com>
2023-08-07 15:38:06 -0600

    Add files via upload

---
+++ models/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+// Allow Promises
+mongoose.Promise = global.Promise
+// Connection
+mongoose.connect(
+	'mongodb+srv://root:root@cluster0.yqpcdpr.mongodb.net/?retryWrites=true&w=majority',
+	{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+)
+// Validation
+mongoose.connection
+	.once('open', () => console.log('Connected to the database!'))
+	.on('error', (err) => console.log('Error with the database!', err))
